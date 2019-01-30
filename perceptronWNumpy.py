096026ebcc05adfa35effb5f3c74165e613f8b3a
Garett MacGowan <garett.macgowan@gmail.com>
2019-01-29 22:42:25 -0500

    Finished assignment

--- perceptronWNumpy.py
+++ perceptronWNumpy.py
@@ -15,21 +15,30 @@ def main():
   # Verify accuracy on test set
   testData = np.genfromtxt('testSeeds.csv', delimiter=',')
   testData = np.insert(testData, 0, 1, axis=1)
-  accuracy, precisionAndRecallArray = test(finalWeights, testData)
+  accuracy, precisionAndRecallArray, confusionMatrixArray = test(finalWeights, testData, False)
   print(accuracy)
-  outputResults(initialWeights, finalWeights, totalIterations, precisionAndRecallArray)
+  outputResults(initialWeights, finalWeights, totalIterations, precisionAndRecallArray, confusionMatrixArray)
 
-def outputResults(initialWeights, finalWeights, totalIterations, precisionAndRecallArray):
+def outputResults(initialWeights, finalWeights, totalIterations, precisionAndRecallArray, confusionMatrixArray):
   text_file = open('output.txt', 'w')
   text_file.write('Initial weights: \n')
   for iw in list(initialWeights):
     text_file.write(str(iw) + '\n')
+  text_file.write('\n')
   text_file.write('Final weights: \n')
   for fw in list(finalWeights):
     text_file.write(str(fw) + '\n')
+  text_file.write('\n')
   text_file.write('Total iterations: \n' + str(totalIterations) + '\n')
   for i in range(0,3):
     text_file.write('Class ' + str(i+1) + ', Precision: ' + str(precisionAndRecallArray[i][0]) + ' Recall: ' + str(precisionAndRecallArray[i][1]) + '\n')
+  text_file.write('\n')
+  text_file.write('Confusion matrix: \n')
+  for j in range(len(confusionMatrixArray)):
+    text_file.write('Class ' + str(j) + '\n')
+    for k in range(len(confusionMatrixArray[j])):
+      text_file.write(str(confusionMatrixArray[j][k]) + '\n')
+    text_file.write('\n')
   text_file.close()
 
 def activate(totalActivation, threshold):
@@ -67,8 +76,8 @@ def train(weights, data):
   currentAccuracy = 0
   currentBestAccuracy = 0
   finalWeights = None
-  # While accuracy has not improved after 10000 iterations
-  while(counter < 10000):
+  # While accuracy has not improved after 100000 iterations
+  while(counter < 100000):
     '''
     Using map which takes in arrays of the same shape (hense np.full) and applies the weightTrainer function to each row
     This trains each neuron to try and be predictive of its class (1, 2, or 3)
@@ -79,7 +88,7 @@ def train(weights, data):
     weights1 = np.mean(np.array(list(map(weightTrainer, np.full((data.shape[0], 8), weights[1]), inputData, classLabels, np.full((data.shape[0], 1), 2)))), 0)
     weights2 = np.mean(np.array(list(map(weightTrainer, np.full((data.shape[0], 8), weights[2]), inputData, classLabels, np.full((data.shape[0], 1), 3)))), 0)
     weights = [weights0, weights1, weights2]
-    currentAccuracy, precisionAndRecallArray = test(weights, data)
+    currentAccuracy, precisionAndRecallArray, confusionMatrixArray = test(weights, data, True)
     # If a set of weights with a better accuracy on the training data is found...
     if (currentAccuracy > currentBestAccuracy):
       currentBestAccuracy = currentAccuracy
@@ -99,68 +108,97 @@ def predictionDecider(class1, class2, class3):
     return 3
   return 1
 
-def getTotalTruePositives(prediction, classLabel):
+def getTotalCorrect(prediction, classLabel):
   if (prediction == classLabel):
     return 1
   else:
     return 0
 
 def getTruePositives(prediction, classLabel, targetClass):
-  if (targetClass == classLabel and prediction == 1):
+  if (targetClass == classLabel and prediction == targetClass):
+    return 1
+  else:
+    return 0
+
+def getTrueNegatives(prediction, classLabel, targetClass):
+  if (targetClass != classLabel and prediction != targetClass):
     return 1
   else:
     return 0
 
 def getFalsePositives(prediction, classLabel, targetClass):
-  if (targetClass != classLabel and prediction == 1):
+  if (targetClass != classLabel and prediction == targetClass):
     return 1
   else:
     return 0
 
 def getFalseNegatives(prediction, classLabel, targetClass):
-  if (targetClass == classLabel and prediction == 0):
+  if (targetClass == classLabel and prediction != targetClass):
     return 1
   else:
     return 0
 
-def test(weights, data):
+def test(weights, data, isTraining):
   inputData = data[0:, 0:-1]
   classLabels = data[0:, -1:].flatten()
   class1 = np.array(list(map(predict, np.full((data.shape[0], 8), weights[0]), inputData)))
   class2 = np.array(list(map(predict, np.full((data.shape[0], 8), weights[1]), inputData)))
   class3 = np.array(list(map(predict, np.full((data.shape[0], 8), weights[2]), inputData)))
 
-  truePositivesC1 = np.sum(getTruePositives(class1, classLabels, np.full(data.shape[0], 1)))
-  truePositivesC2 = np.sum(getTruePositives(class2, classLabels, np.full(data.shape[0], 2)))
-  truePositivesC3 = np.sum(getTruePositives(class3, classLabels, np.full(data.shape[0], 3)))
+  prediction = predictionDecider(class1, class2, class3)
 
-  falsePositivesC1 = np.sum(getFalsePositives(class1, classLabels, np.full(data.shape[0], 1)))
-  falsePositivesC2 = np.sum(getFalsePositives(class2, classLabels, np.full(data.shape[0], 2)))
-  falsePositivesC3 = np.sum(getFalsePositives(class3, classLabels, np.full(data.shape[0], 3)))
+  precisionAndRecallArray = []
+  confusionMatrixArray = []
 
-  falseNegativesC1 = np.sum(getFalseNegatives(class1, classLabels, np.full(data.shape[0], 1)))
-  falseNegativesC2 = np.sum(getFalseNegatives(class1, classLabels, np.full(data.shape[0], 2)))
-  falseNegativesC3 = np.sum(getFalseNegatives(class1, classLabels, np.full(data.shape[0], 3)))
+  if (not isTraining):
 
-  class1Precision = truePositivesC1 / (truePositivesC1 + falsePositivesC1)
-  class2Precision = truePositivesC2 / (truePositivesC2 + falsePositivesC2)
-  class3Precision = truePositivesC3 / (truePositivesC3 + falsePositivesC3)
+    truePositivesC1 = np.sum(getTruePositives(prediction, classLabels, np.full(data.shape[0], 1)))
+    truePositivesC2 = np.sum(getTruePositives(prediction, classLabels, np.full(data.shape[0], 2)))
+    truePositivesC3 = np.sum(getTruePositives(prediction, classLabels, np.full(data.shape[0], 3)))
 
-  class1Recall = truePositivesC1 / (truePositivesC1 + falseNegativesC1)
-  class2Recall = truePositivesC2 / (truePositivesC2 + falseNegativesC2)
-  class3Recall = truePositivesC3 / (truePositivesC3 + falseNegativesC3)
+    trueNegativesC1 = np.sum(getTrueNegatives(prediction, classLabels, np.full(data.shape[0], 1)))
+    trueNegativesC2 = np.sum(getTrueNegatives(prediction, classLabels, np.full(data.shape[0], 2)))
+    trueNegativesC3 = np.sum(getTrueNegatives(prediction, classLabels, np.full(data.shape[0], 3)))
 
-  precisionAndRecallArray = [[class1Precision, class1Recall], [class2Precision, class2Recall], [class3Precision, class3Recall]]
+    falsePositivesC1 = np.sum(getFalsePositives(prediction, classLabels, np.full(data.shape[0], 1)))
+    falsePositivesC2 = np.sum(getFalsePositives(prediction, classLabels, np.full(data.shape[0], 2)))
+    falsePositivesC3 = np.sum(getFalsePositives(prediction, classLabels, np.full(data.shape[0], 3)))
 
-  totalTruePositivesMatrix = getTotalTruePositives(predictionDecider(class1, class2, class3), classLabels)
-  accuracy = np.sum(totalTruePositivesMatrix) / totalTruePositivesMatrix.shape[0]
-  return accuracy, precisionAndRecallArray
+    falseNegativesC1 = np.sum(getFalseNegatives(prediction, classLabels, np.full(data.shape[0], 1)))
+    falseNegativesC2 = np.sum(getFalseNegatives(prediction, classLabels, np.full(data.shape[0], 2)))
+    falseNegativesC3 = np.sum(getFalseNegatives(prediction, classLabels, np.full(data.shape[0], 3)))
+
+
+    class1Precision = truePositivesC1 / (truePositivesC1 + falsePositivesC1)
+    class2Precision = truePositivesC2 / (truePositivesC2 + falsePositivesC2)
+    class3Precision = truePositivesC3 / (truePositivesC3 + falsePositivesC3)
+
+    class1Recall = truePositivesC1 / (truePositivesC1 + falseNegativesC1)
+    class2Recall = truePositivesC2 / (truePositivesC2 + falseNegativesC2)
+    class3Recall = truePositivesC3 / (truePositivesC3 + falseNegativesC3)
+
+    precisionAndRecallArray = [[class1Precision, class1Recall], [class2Precision, class2Recall], [class3Precision, class3Recall]]
+
+    confusionMatrixArray = [[[truePositivesC1, falsePositivesC1],
+                            [falseNegativesC1, trueNegativesC1]],
+                            
+                            [[truePositivesC2, falsePositivesC2],
+                            [falseNegativesC2, trueNegativesC2]],
+                            
+                            [[truePositivesC3, falsePositivesC3],
+                            [falseNegativesC3, trueNegativesC3]]]
+
+  totalPositivesMatrix = getTotalCorrect(prediction, classLabels)
+  accuracy = np.sum(totalPositivesMatrix) / totalPositivesMatrix.shape[0]
+  
+  return accuracy, precisionAndRecallArray, confusionMatrixArray
 
 # Vectorizing these two functions (essentially creates a for loop)
 getTruePositives = np.vectorize(getTruePositives)
+getTrueNegatives = np.vectorize(getTrueNegatives)
 getFalsePositives = np.vectorize(getFalsePositives)
 getFalseNegatives = np.vectorize(getFalseNegatives)
-getTotalTruePositives = np.vectorize(getTotalTruePositives)
+getTotalCorrect = np.vectorize(getTotalCorrect)
 predictionDecider = np.vectorize(predictionDecider)
 
 main()
